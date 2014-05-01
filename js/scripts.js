6a9a7014b77b8fad168d100730051f45ce47f25b
sergethompson <serge.thompson@gmail.com>
2014-05-01 17:14:34 -0400

    clean up

--- js/scripts.js
+++ js/scripts.js
@@ -53,81 +53,48 @@ if( ( !gammaState  && !gammaStateToo ) && domState === "ON"){
 	elementp.parentNode.removeChild(elementp);
 	domState = "OFF";
 
-	};
-  
-  	if( (gammaStateToo || gammaState) && betaState && domState === "OFF" ){
-  			domState = "ON";
-  			$.getJSON( "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"", function( data ) {
-  				var overlay = document.createElement("div");
-  				overlay.setAttribute("id","overlay");
-  				overlay.setAttribute("class", "overlay");
-  				overlay.style.position="fixed";
-  				overlay.style.backgroundColor="black";
-  				overlay.style.opacity=".7";
-  				overlay.style.width="100%";
-  				overlay.style.height="100%";
-  				overlay.style.zIndex="1000";
-  				overlay.style.top="0";
-  				overlay.style.left="0";
-  				overlay.style.filter="alpha(opacity=70)";
-  				document.body.appendChild(overlay);
+};
+
+if( (gammaStateToo || gammaState) && betaState && domState === "OFF" ){
+	domState = "ON";
+	$.getJSON( "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"", function( data ) {
+		var overlay = document.createElement("div");
+		overlay.setAttribute("id","overlay");
+		overlay.setAttribute("class", "overlay");
+		overlay.style.position="fixed";
+		overlay.style.backgroundColor="black";
+		overlay.style.opacity=".7";
+		overlay.style.width="100%";
+		overlay.style.height="100%";
+		overlay.style.zIndex="1000";
+		overlay.style.top="0";
+		overlay.style.left="0";
+		overlay.style.filter="alpha(opacity=70)";
+		document.body.appendChild(overlay);
 
 
 
 
-        var productOverlay = document.createElement("div");
-        productOverlay.setAttribute("id","productOverlay");
-        productOverlay.setAttribute("class", "productOverlay");
-        productOverlay.style.backgroundColor="#4bccff";
-        productOverlay.style.color="white";
-        productOverlay.style.fontFamily='Source Sans Pro';
-        productOverlay.style.textAlign="center";
-        productOverlay.style.fontSize="30px";
-        productOverlay.style.position="fixed";
-        productOverlay.style.width="650px";
-        productOverlay.style.height="250px";
-        productOverlay.style.zIndex="1010";
-        productOverlay.style.top="50%";
-        productOverlay.style.left="50%";
-        productOverlay.style.marginLeft="-325px";
-        productOverlay.style.marginTop="-125px";
-        document.body.appendChild(productOverlay);
-        document.getElementById("productOverlay").innerHTML="Weather: " + data.weather[0].description
-          + "<br>" + "Temp: " + (data.main.temp * 1.8 - 459.67).toFixed(2) + " Location: " + data.name +
-          "<br><br> You Won a Free Guinness! CODE: "+ Math.floor((Math.random() * 10000) + 1);
-
-        
-
-
-	 // alert("Hello your lat lon " + latitude.toFixed(2) +" : " 
-	 // 	+ longitude.toFixed(2) + " Temp: "+(dataX.main.temp * 1.8 - 459.67).toFixed(2)
-	 // 	 +" Weather: "+dataX.weather[0].description);
-
-
-
-// var r=confirm("Hello your lat lon " + latitude.toFixed(2) +" : " 
-// 	+ longitude.toFixed(2) + " Temp: "+(dataX.main.temp * 1.8 - 459.67).toFixed(2)
-// 	+" Weather: "+dataX.weather[0].description);
-// if (r==true)
-// {
-// 	var elemento = document.getElementById("overlay");
-// 	elemento.parentNode.removeChild(elemento);
-// }
-// else
-// {
-
-// };
-
-
-
-
-
-
-
-
-
-	 // 		var elemento = document.getElementById("overlay");
-		// elemento.parentNode.removeChild(elemento);
+		var productOverlay = document.createElement("div");
+		productOverlay.setAttribute("id","productOverlay");
+		productOverlay.setAttribute("class", "productOverlay");
+		productOverlay.style.backgroundColor="#4bccff";
+		productOverlay.style.color="white";
+		productOverlay.style.fontFamily='Source Sans Pro';
+		productOverlay.style.textAlign="center";
+		productOverlay.style.fontSize="30px";
+		productOverlay.style.position="fixed";
+		productOverlay.style.width="650px";
+		productOverlay.style.height="250px";
+		productOverlay.style.zIndex="1010";
+		productOverlay.style.top="50%";
+		productOverlay.style.left="50%";
+		productOverlay.style.marginLeft="-325px";
+		productOverlay.style.marginTop="-125px";
+		document.body.appendChild(productOverlay);
+		document.getElementById("productOverlay").innerHTML="Weather: " + data.weather[0].description
+		+ "<br>" + "Temp: " + (data.main.temp * 1.8 - 459.67).toFixed(2) + " Location: " + data.name +
+		"<br><br> You Won a Free Guinness! CODE: "+ Math.floor((Math.random() * 10000) + 1);
 	});
 };
 
@@ -136,15 +103,6 @@ if( ( !gammaState  && !gammaStateToo ) && domState === "ON"){
 };
 
 getLocation();
-// setTimeout(function(){
-// 	alert("Hello Serge your lat lon");
-//   			trigger = false;
-//   		}, 3000);
 
-
-// $.getJSON( "http://api.openweathermap.org/data/2.5/weather?lat=41.04543&lon=-73.57633", function( data ) {
-// 	console.log(data);
-// 	dataX = data;
-// });
 
 
