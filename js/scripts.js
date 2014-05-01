f0baebe948b2a0caf9ac8984461022ee4655dcf1
sergethompson <serge.thompson@gmail.com>
2014-04-30 22:28:36 -0400

    some crazzzzzy stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+var latitude;
+var longitude;
+var countDown = false;
+
 $( window ).scroll(function() {
   var scroll = $(window).scrollTop();
   $(".resume").css({"right": 2*scroll});
@@ -6,3 +10,48 @@ $( window ).scroll(function() {
   $(".github").css({"right": -2*scroll});
 
 });
+
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    }
+  else{console.log("Geolocation is not supported by this browser.");}
+  }
+function showPosition(position)
+  {
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude; 
+  };
+
+
+
+
+window.addEventListener("deviceorientation", handleOrientation, true);
+
+
+function handleOrientation(event) {
+  var absolute = event.absolute;
+  var alpha    = event.alpha;
+  var beta     = event.beta;
+  var gamma    = event.gamma;
+
+
+  // 
+
+  if (!countDown) {
+  	if(-55 < gamma < -35){
+  		setTimeout(function(){alert("Hello")
+  			countDown = false;
+  		}, 3000);
+  		countDown = true;
+  	};
+
+  }; 
+
+
+  // Do stuff with the new orientation data
+};
+
+
