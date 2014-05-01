b359ad38d960e86283647aae1d88207f46510de2
sergethompson <serge.thompson@gmail.com>
2014-05-01 00:40:35 -0400

    overlay

--- js/scripts.js
+++ js/scripts.js
@@ -51,7 +51,51 @@ function handleOrientation(event) {
   			$.getJSON( "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"", function( data ) {
 	 console.log(data)
 	 dataX = data;
-	 alert("Hello your lat lon " + latitude.toFixed(2) +" : " + longitude.toFixed(2) + " Temp: "+(dataX.main.temp * 1.8 - 459.67).toFixed(2) +" Weather: "+dataX.weather[0].description);
+
+var overlay = document.createElement("div");
+        overlay.setAttribute("id","overlay");
+        overlay.setAttribute("class", "overlay");
+        overlay.style.position="fixed";
+        overlay.style.backgroundColor="black";
+        overlay.style.opacity=".7";
+        overlay.style.width="100%";
+        overlay.style.height="100%";
+        overlay.style.zIndex="1000";
+        overlay.style.top="0";
+        overlay.style.left="0";
+        overlay.style.filter="alpha(opacity=70)";
+        document.body.appendChild(overlay);
+
+
+        // var productOverlay = document.createElement("div");
+        // productOverlay.setAttribute("id","productOverlay");
+        // productOverlay.setAttribute("class", "productOverlay");
+        // productOverlay.style.backgroundColor="#4bccff";
+        // productOverlay.style.color="white";
+        // productOverlay.style.fontFamily="Proxima";
+        // productOverlay.style.textAlign="center";
+        // productOverlay.style.fontSize="30px";
+        // productOverlay.style.position="fixed";
+        // productOverlay.style.width="520px";
+        // productOverlay.style.height="160px";
+        // productOverlay.style.zIndex="1010";
+        // productOverlay.style.top="50%";
+        // productOverlay.style.left="50%";
+        // productOverlay.style.marginLeft="-260px";
+        // productOverlay.style.marginTop="-80px";
+        // document.body.appendChild(productOverlay);
+        // document.getElementById("productOverlay").innerHTML="<br>" +"Items in Cart: " +cartTotal+ "<br>" +"<br>" +"Subtotal Price: "+subtotal;
+
+
+
+
+
+
+	 alert("Hello your lat lon " + latitude.toFixed(2) +" : " 
+	 	+ longitude.toFixed(2) + " Temp: "+(dataX.main.temp * 1.8 - 459.67).toFixed(2)
+	 	 +" Weather: "+dataX.weather[0].description);
+	 		var elemento = document.getElementById("overlay");
+		elemento.parentNode.removeChild(elemento);
 	});
 
   			
