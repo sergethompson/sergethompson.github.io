9d6185d341f6e137573ff94cb36146e6b15e9bb7
sergethompson <serge.thompson@gmail.com>
2014-05-23 11:13:18 -0400

    size change 4

--- js/scripts.js
+++ js/scripts.js
@@ -162,7 +162,7 @@ window.setInterval(function(){
             latitude:la,
             longitude:lo,
 				    time:Date(),
-				    radius: 3,
+				    radius: 4,
 				    fillKey: 'VISITOR'
         };
         $.ajax({
